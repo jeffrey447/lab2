7979925e43d4b99c4a7b36692fd5780c36b8fd0b
Jeffrey Ha <jha@ucsd.edu>
2021-01-14 03:30:53 -0800

    added deselect feature

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,20 @@
-$(document).ready(function() {
-    initializePage();
-});
-
 function projectClick(e) {
     // prevent the page from reloading
     e.preventDefault();
 
-    $(this).css("background-color", "#7fff00");
+    let styleattr = $(this).attr('style');
+    if (typeof styleattr !== typeof undefined && styleattr !== false)
+        $(this).removeAttr('style');
+    else
+        $(this).css("background-color", "#7fff00");
 }
 
 function initializePage() {
     $('#testjs').click(function(e) {
-        $('.jumbotron h1').text("Javascript is connected");
+        $('.jumbotron h1').text('Javascript is connected');
     });
 
     $('a.thumbnail').click(projectClick);
 }
+
+$(document).ready(initializePage);
