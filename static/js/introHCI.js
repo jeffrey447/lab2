5b5c1408f7b41e12af0423a3505a5f2dbe15141f
Jeffrey Ha <jha@ucsd.edu>
2021-01-13 23:32:36 -0800

    added link handling

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,8 +2,17 @@ $(document).ready(function() {
     initializePage();
 });
 
+function projectClick(e) {
+    // prevent the page from reloading
+    e.preventDefault();
+
+    $(this).css("background-color", "#7fff00");
+}
+
 function initializePage() {
     $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
+        $('.jumbotron h1').text("Javascript is connected");
+    });
+
+    $('a.thumbnail').click(projectClick);
 }
